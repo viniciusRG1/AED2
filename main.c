87aa48cff727eddbdd2ee34f66b3af9549594396
vinicius <viniciusxsantosx@hotmail.com.br>
2025-08-21 14:45:01 -0300

    procura binária v 0.0.2, tratamento ta músisca completa

--- main.c
+++ main.c
@@ -2,60 +2,104 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define tamanhoPalavra 20
+#include <ctype.h>
+#define tamanhoPalavra 21
 #define listaDeMusicas "listaDeMusica"
+#define tamanhoDaEstrofe 101
 
 typedef struct {
-    char nomeMusica[20];
-    char nomeDoAutor[20];
-    char *letra;
+    char nomeMusica[tamanhoPalavra];
+    char nomeDoAutor[tamanhoPalavra];
+    char estrofe[tamanhoDaEstrofe];
+    char **palavras;
+    int numPalavras;
 } estruturaMusica;
 
 void removerBarraN(char *valComBarraN){
     valComBarraN[strcspn(valComBarraN,"\n")] = 0;
 }
 
-estruturaMusica carregarArquivo(const char* musicaArquivo){
+void tratamentoMusica(estruturaMusica *infos){
+    int tamanho = 2;
+    for(int i = 0; i < infos->numPalavras; i++){
+        int tamanhoAtual = strlen(infos->palavras[i]);
+        if(tamanho >= tamanhoAtual){
+            free(infos->palavras[i]);
+            for(int aux = i; aux<infos->numPalavras - 1; aux++){
+                infos->palavras[aux] = infos->palavras[aux + 1];
+                infos->numPalavras--;
+            }
+        }
+        for(int aux = 0; infos->palavras[i][aux]; aux++){
+            infos->palavras[i][aux] = tolower(infos->palavras[i][aux]);
+        }
+    }
+    char **aux = realloc(infos->palavras, infos->numPalavras * sizeof(char *));
+
+ }
+
+estruturaMusica carregarArquivo(const char* musicaArquivo) {
     estruturaMusica musica;
-    musica.letra = NULL;
+    musica.palavras = NULL;
+    musica.numPalavras = 0;
+    musica.estrofe[0] = '\0';
+    musica.nomeMusica[0] = '\0';
     FILE *arquivo = fopen(musicaArquivo, "r");
-    if(arquivo == NULL){
+    if (arquivo == NULL) {
         perror("Erro ao abrir arquivo");
         strcpy(musica.nomeMusica, "ERRO");
         return musica;
     }
-    if(fgets(musica.nomeMusica, sizeof(musica.nomeMusica), arquivo)){
+    if (fgets(musica.nomeMusica, sizeof(musica.nomeMusica), arquivo)) {
         removerBarraN(musica.nomeMusica);
     }
-    if(fgets(musica.nomeDoAutor, sizeof(musica.nomeDoAutor), arquivo)){
+    if (fgets(musica.nomeDoAutor, sizeof(musica.nomeDoAutor), arquivo)) {
         removerBarraN(musica.nomeDoAutor);
     }
-    size_t capacidade = 1024;
-    musica.letra = (char*)malloc(capacidade);
-    if(musica.letra == NULL){
-        perror("falha em carregar a musica");
-        fclose(arquivo);
-        return musica;
-    }
-    musica.letra[0];
+    size_t capacidadeLetra = 2048;
+    char *letraCompleta = (char*) malloc(capacidadeLetra);
+    letraCompleta[0] = '\0';
     char linhaBuffer[512];
-    while (fgets(linhaBuffer, sizeof(linhaBuffer), arquivo) != NULL){
-        if(strlen(musica.letra)+strlen(linhaBuffer)+ 1 > capacidade){
-            capacidade *= 2;
-            char *temp = (char*)realloc(musica.letra, capacidade);
-            if(temp == NULL){
-                perror("falha a realocao da letra");
-                free(musica.letra);
-                musica.letra = NULL;
-                break;
+    while (fgets(linhaBuffer, sizeof(linhaBuffer), arquivo) != NULL) {
+        if (strlen(letraCompleta) + strlen(linhaBuffer) + 1 > capacidadeLetra) {
+            capacidadeLetra *= 2;
+            char *aux = (char*) realloc(letraCompleta, capacidadeLetra);
+            if(aux == NULL) {
+                    break;
             }
-            musica.letra = temp;
+            letraCompleta = aux;
         }
-        strcat(musica.letra, linhaBuffer);
+        strcat(letraCompleta, linhaBuffer);
     }
     fclose(arquivo);
+    char *fimEstrofe = strstr(letraCompleta, "\n\n");
+    int tamanhoEstrofe = 0;
+    if (fimEstrofe != NULL) {
+        tamanhoEstrofe = fimEstrofe - letraCompleta;
+    } else {
+        tamanhoEstrofe = strlen(letraCompleta);
+    }
+    int tamanho_copia = tamanhoEstrofe < tamanhoDaEstrofe ? tamanhoEstrofe : tamanhoDaEstrofe;
+    strncpy(musica.estrofe, letraCompleta, tamanho_copia);
+    musica.estrofe[tamanho_copia] = '\0';
+    char *letraVetor = strdup(letraCompleta);
+    size_t capacidadePalavras = 50;
+    musica.palavras = (char**) malloc(capacidadePalavras * sizeof(char*));
+    char *palavra = strtok(letraVetor, "\n.,!?");
+    while (palavra != NULL) {
+        if (musica.numPalavras >= capacidadePalavras) {
+            capacidadePalavras *= 2;
+            musica.palavras = (char**) realloc(musica.palavras, capacidadePalavras * sizeof(char*));
+        }
+        char *novaPalavra = strdup(palavra);
+        musica.palavras[musica.numPalavras] = novaPalavra;
+        musica.numPalavras++;
+
+        palavra = strtok(NULL, "\n.,!?");
+    }
     return musica;
 }
+
 
 char* escolherPalavra(){
     char* palavra = (char*)malloc(tamanhoPalavra*sizeof(char));
@@ -80,15 +124,18 @@ void menu(){
     printf("5 - comparacao vetor e arvore AVL\n");
     printf("6 - comparacao entre arvores");
     printf("7 - comparacao entre vetor, arvore binaria e arvore AVL\n");
+    printf("8 - freqencia de uma palavra\n");
     opcoes();
 
 }
+
+void casos(int opcao);
 
 void opcoes(){
     int escolha;
     menu();
     scanf("qual opcao ?", &escolha);
-    menu();
+    casos(escolha);
 }
 
 void pesquisaBinario(){
@@ -98,6 +145,7 @@ void arvoreBinario(){
 }
 
 void arvoreAVL(){
+
 }
 
 void casos(int opcoes){
@@ -178,6 +226,8 @@ void casos(int opcoes){
         printf("o tempo gasto por arvore binarioa foi%f\n", tempoGastoArvoreB);
         printf("o tempo gasto por arvore AVL foi%f\n", tempoGastoArvoreAVL);
         break;
+    case 8:
+
     }
 }
 
